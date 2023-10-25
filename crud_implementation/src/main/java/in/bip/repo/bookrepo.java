3522e53cad931fc042fd79870aa426d0b2149b8c
bipul <bipul@DESKTOP-ITU7NP7>
2023-10-25 15:37:45 +0530

    This is a commit

--- crud_implementation/src/main/java/in/bip/repo/bookrepo.java
+++
@@ -1,9 +0,0 @@
-package in.bip.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import in.bip.entity.Book;
-
-public interface bookrepo extends CrudRepository<Book, Integer>{
-
-}
